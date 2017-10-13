118a78d24468bcbbde2e2c5daf42b711db703d6e
Aurimas <aurimas@darguzis.com>
2017-10-13 11:37:42 +0100

    added floating point numbers test

---
+++ __tests__/commonMatchers.test.js
@@ -0,0 +1,37 @@
+// truthiness
+test('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+});
+
+test('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy()
+});
+
+// numbers
+test('two plus two', () => {
+    const value = 2 + 2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(4.5);
+
+    // toBe and toEqual are equivalent for numbers
+    expect(value).toBe(4);
+    expect(value).toEqual(4);
+});
+
+test('adding floating point numbers', () => {
+    const value = 0.1 + 0.2;
+    expect(value).not.toBe(0.3); // It isn't! Because rounding error
+    expect(value).toBeCloseTo(0.3); // This works!
+});
